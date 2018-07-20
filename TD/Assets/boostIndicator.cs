b1a7dcb29fe75adf8986dac67607cc2fb1167f57
prestoncyberdog <33162385+prestoncyberdog@users.noreply.github.com>
2018-07-20 15:56:40 -0500

    does this do anything

---
+++ TD/Assets/boostIndicator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boostIndicator : MonoBehaviour {
+
+	public Sprite damage;
+	public Sprite speed;
+	public Sprite range;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
